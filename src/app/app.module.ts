53afa473b8acacd3ee42af9cb1e04c93c18cbeee
Hamilton Lima <hamilton.lima@indexexchange.com>
2018-06-17 09:10:41 -0400

    Removes ngix-socket.io dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestWebsocketComponent } from './test-websocket/test-websocket.component';
 
-const config: SocketIoConfig = { url: 'http://localhost:5000/ws', options: {} };
-
 @NgModule({
   declarations: [AppComponent, TestWebsocketComponent],
-  imports: [BrowserModule, AppRoutingModule, SocketIoModule.forRoot(config)],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [TestWebsocketComponent],
   bootstrap: [AppComponent]
 })
