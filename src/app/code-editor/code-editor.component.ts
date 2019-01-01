e80c67fce97b5cacc7f7ce044091b8ad109afb33
Hamilton Lima <hamilton.lima@gmail.com>
2019-01-01 16:38:59 -0500

    Creates Code editor component and updates Luchador from editor

--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import "brace/theme/solarized_dark";
+import "brace/mode/lua";
 
 @Component({
-  selector: 'app-code-editor',
-  templateUrl: './code-editor.component.html',
-  styleUrls: ['./code-editor.component.css']
+  selector: "app-code-editor",
+  templateUrl: "./code-editor.component.html",
+  styleUrls: ["./code-editor.component.css"]
 })
 export class CodeEditorComponent implements OnInit {
+  options: any = {
+    maxLines: 15,
+    minLines: 15,
+    printMargin: false,
+    fontSize: 20
+  };
 
-  @Input() script:string = "";
+  @Input() script: string = "";
+  @Output() onChange = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onEditorChange(event) {
+    console.log("onchange", event);
+    this.onChange.next( event );
   }
-
 }
--- src/app/luchador/luchador.component.ts
+++ src/app/luchador/luchador.component.ts
@@ -11,13 +11,14 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class LuchadorComponent implements OnInit {
   luchador: MainLuchador;
-
-  start: MainCode;
-  onRepeat: MainCode;
-  onGotDamage: MainCode;
-  onFound: MainCode;
-  onHitOther: MainCode;
-  onHitWall: MainCode;
+  codes = {
+    start: <MainCode>{},
+    onRepeat: <MainCode>{},
+    onGotDamage: <MainCode>{},
+    onFound: <MainCode>{},
+    onHitOther: <MainCode>{},
+    onHitWall: <MainCode>{}
+  };
 
   constructor(private route: ActivatedRoute) {
     this.luchador = {};
@@ -28,26 +29,48 @@ export class LuchadorComponent implements OnInit {
     this.luchador = data.luchador;
 
     console.log("luchador found", this.luchador);
-    this.start = this.getCode("start");
-    this.onRepeat = this.getCode("onRepeat");
-    this.onGotDamage = this.getCode("onGotDamage");
-    this.onFound = this.getCode("onFound");
-    this.onHitOther = this.getCode("onHitOther");
-    this.onHitWall = this.getCode("onHitWall");
+    for( var key in this.codes){
+      this.codes[key] = this.getCode(key); 
+    }
+    // this.codes.start = 
+    // this.codes.onRepeat = this.getCode("onRepeat");
+    // this.codes.onGotDamage = this.getCode("onGotDamage");
+    // this.onFound = this.getCode("onFound");
+    // this.onHitOther = this.getCode("onHitOther");
+    // this.onHitWall = this.getCode("onHitWall");
   }
 
   getCode(event: string): MainCode {
-    let result = this.luchador.codes.find((code: MainCode) => {
-      if (code.event == event) {
-        return true;
-      }
-      return false;
-    });
+    let result = this.findCodeByEventName(event);
 
     if (!result) {
       result = <MainCode>{};
     }
 
     return result;
+  }
+
+  findCodeByEventName(event: string) {
+    return this.luchador.codes.find((code: MainCode) => {
+      if (code.event == event) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  updateCode(event: string, script: string) {
+    console.log("updateCode", event, script);
+
+    let code = this.findCodeByEventName(event);
+
+    if (code) {
+      code.script = script;
+    } else {
+      code = <MainCode>{ event: event, script: script };
+      this.luchador.codes.push(code);
+    }
+
+    this.codes[event] = code;
   }
 }
