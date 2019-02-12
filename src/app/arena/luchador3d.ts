a6a9b1613174c6f8513b5ebcb4acfbec1914f15b
hamilton.lima <hamilton.lima@gmail.com>
2019-02-12 09:13:58 -0500

    Integrate Luchador3D with MeshLoader

--- src/app/arena/luchador3d.ts
+++ src/app/arena/luchador3d.ts
@@ -2,6 +2,7 @@ import { SharedConstants } from "./shared.constants";
 import * as BABYLON from "babylonjs";
 import { Luchador } from "../watch-match/watch-match.model";
 import { Base3D } from "./base3D";
+import { MeshLoader } from "./mesh.loader";
 
 export class Luchador3D extends Base3D {
   getName(): string {
@@ -48,58 +49,76 @@ export class Luchador3D extends Base3D {
 
     let self = this;
 
-    BABYLON.SceneLoader.ImportMesh(
-      "",
-      "assets/",
-      "robolucha_char03.babylon",
-      scene,
-      function(newMeshes, particleSystems) {
-        console.log("[Luchador3D] imported meshes luchador", newMeshes);
+    let charLoader = MeshLoader.load(scene,'robolucha_char03.babylon','robolucha_retopo');
+    let baseLoader = MeshLoader.load(scene,'vehicle_base_anim01.babylon','vehicle_base');
+    let turretLoader = MeshLoader.load(scene,'vehicle_turret_anim01.babylon','vehicle_turret');
+    
+    charLoader.then( mesh => {
+      mesh.isVisible = true;
+      mesh.parent = self.turret;
+      self.character = mesh;
+      self.character.id = self.getName() + ".character";
+    })
 
-        newMeshes.forEach(mesh => {
-          console.log("[Luchador3D] mesh.name", mesh.name);
-          if (mesh.name == "robolucha_retopo") {
-            mesh.parent = self.turret;
-            self.character = mesh;
-            self.character.id = self.getName() + ".character";
-          }
-        });
-      }
-    );
+    baseLoader.then( mesh => {
+      mesh.isVisible = true;
+      mesh.parent = self.base;
+    });
 
-    BABYLON.SceneLoader.ImportMesh(
-      "",
-      "assets/",
-      "base_texture.babylon",
-      scene,
-      function(newMeshes, particleSystems) {
-        console.log("[Luchador3D] imported meshes base", newMeshes);
+    turretLoader.then( mesh => {
+      mesh.isVisible = true;
+      mesh.parent = self.turret;
+    });
 
-        newMeshes.forEach(mesh => {
-          console.log("[Luchador3D] mesh.name", mesh.name);
-          if (mesh.name == "vehicle_base") {
-            mesh.parent = self.base;
-          }
-        });
-      }
-    );
+    // BABYLON.SceneLoader.ImportMesh(
+    //   "",
+    //   "assets/",
+    //   "robolucha_char03.babylon",
+    //   scene,
+    //   function (newMeshes, particleSystems) {
+    //     console.log("[Luchador3D] imported meshes luchador", newMeshes);
 
-    BABYLON.SceneLoader.ImportMesh(
-      "",
-      "assets/",
-      "vehicleTurret_textured.babylon",
-      scene,
-      function(newMeshes, particleSystems) {
-        console.log("[Luchador3D] imported meshes turret", newMeshes);
+    //     newMeshes.forEach(mesh => {
+    //       console.log("[Luchador3D] mesh.name", mesh.name);
+    //       if (mesh.name == "robolucha_retopo") {
+    //       }
+    //     });
+    //   }
+    // );
 
-        newMeshes.forEach(mesh => {
-          console.log("[Luchador3D] mesh.name", mesh.name);
-          if (mesh.name == "vehicle_turret") {
-            mesh.parent = self.turret;
-          }
-        });
-      }
-    );
+    // BABYLON.SceneLoader.ImportMesh(
+    //   "",
+    //   "assets/",
+    //   "base_texture.babylon",
+    //   scene,
+    //   function (newMeshes, particleSystems) {
+    //     console.log("[Luchador3D] imported meshes base", newMeshes);
+
+    //     newMeshes.forEach(mesh => {
+    //       console.log("[Luchador3D] mesh.name", mesh.name);
+    //       if (mesh.name == "vehicle_base") {
+    //         mesh.parent = self.base;
+    //       }
+    //     });
+    //   }
+    // );
+
+    // BABYLON.SceneLoader.ImportMesh(
+    //   "",
+    //   "assets/",
+    //   "vehicleTurret_textured.babylon",
+    //   scene,
+    //   function (newMeshes, particleSystems) {
+    //     console.log("[Luchador3D] imported meshes turret", newMeshes);
+
+    //     newMeshes.forEach(mesh => {
+    //       console.log("[Luchador3D] mesh.name", mesh.name);
+    //       if (mesh.name == "vehicle_turret") {
+    //         mesh.parent = self.turret;
+    //       }
+    //     });
+    //   }
+    // );
 
     let labelColor = BABYLON.Color3.FromHexString("#DDDDDD");
     this.addLabel(luchador.state.name, -100, labelColor);
