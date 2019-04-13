bccc571c514cdd60996e1494137afc50428ff214
Mateus Lacerda <mateus.sateles@gmail.com>
2019-04-13 12:30:35 -0300

    Display messages from luchador console

--- src/app/watch-match/watch-match.component.ts
+++ src/app/watch-match/watch-match.component.ts
@@ -6,6 +6,8 @@ import { SharedStateService } from "../shared-state.service";
 import { Subscription, Subject } from "rxjs";
 import { MatchState, GameDefinition } from "./watch-match.model";
 
+const MESSAGE_TIMEOUT = 3000;
+const MAX_MESSAGES = 5;
 @Component({
   selector: "app-watch-match",
   templateUrl: "./watch-match.component.html",
@@ -17,6 +19,8 @@ export class WatchMatchComponent implements OnInit, OnDestroy {
 
   luchador: MainLuchador;
   message: string;
+  userMessage: any;
+  messageList: Array<any>;
   matchState: MatchState;
   subscription: Subscription;
   onMessage: Subscription;
@@ -41,6 +45,7 @@ export class WatchMatchComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.luchador = this.route.snapshot.data.luchador;
+    this.messageList =[];
     console.log("watch match oninit", this.luchador);
 
     this.subscription = this.service.ready.subscribe(() => {
@@ -55,21 +60,67 @@ export class WatchMatchComponent implements OnInit, OnDestroy {
         console.log("watch details", details);
         this.onMessage = this.service.watch(details).subscribe(message => {
           this.message = message;
-          const matchStateCandidate: MatchState = JSON.parse(this.message);
-
-          // TODO: replace this by the proper message type from the publisher
-          if( matchStateCandidate.clock ){
-            this.matchState = matchStateCandidate;
+          let parsedMessage = JSON.parse(this.message);
+          if(parsedMessage.type == "Match State") {
+            this.matchState = parsedMessage.message;
             this.matchStateSubject.next(this.matchState);
-          } else { 
-            console.log("other event", this.message);
           }
+          else if (parsedMessage.type == "Message") {
 
+            if (parsedMessage.message.luchadorID == this.luchador.id) {
+              this.userMessage = parsedMessage.message;
+              this.chooseBoxType();
+            }
+            this.processMessageList();
+          }
         });
       }
     });
 
     this.service.connect();
+  }
+
+  private chooseBoxType() {
+    switch (this.userMessage.type) {
+      case "debug": {
+        this.userMessage.boxType = "info";
+        break;
+      }
+      case "say": {
+        this.userMessage.boxType = "secondary";
+        break;
+      }
+      case "warning": {
+        this.userMessage.boxType = "warning";
+        break;
+      }
+      case "danger": {
+        this.userMessage.boxType = "danger";
+        break;
+      }
+    }
+  }
+
+  private processMessageList() {
+    this.cleanMessageList();
+
+    let l = this.messageList.push(this.userMessage);
+    //max is 5 elements, remove oldest
+    if (l > MAX_MESSAGES) {
+      this.messageList.splice(0, 1);
+      l = this.messageList.length;
+    }
+    setTimeout(() => this.messageList[l - 1] = null, MESSAGE_TIMEOUT);
+    setTimeout(() => this.cleanMessageList(), MESSAGE_TIMEOUT * 2);
+  }
+
+  private cleanMessageList() {
+    //removing null elements
+    for (let i = 0; i < this.messageList.length; i++) {
+      if (!this.messageList[i]) {
+        this.messageList.splice(i, 1);
+      }
+    }
   }
 
   ngOnDestroy(): void {
@@ -92,5 +143,12 @@ export class WatchMatchComponent implements OnInit, OnDestroy {
     if (a.state.score > b.state.score)
       return 1;
     return 0;
+  }
+
+  getUserMessages(){
+    if (this.messageList){
+      return this.messageList;
+    }
+    return [];
   }
 }
