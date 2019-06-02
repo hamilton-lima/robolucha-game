5feffa837c1f8c3456726e81acd80b08ec3f690b
Hamilton Lima <hamilton.lima@gmail.com>
2019-06-01 23:04:02 -0400

    Adds gamedefinition selection

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
 import { Router } from "@angular/router";
+import { DefaultService, MainGameDefinition } from "../sdk";
+import { NgxSpinnerService } from "ngx-spinner";
+
+export interface Selection {
+  position: number;
+  total: number;
+  value: number;
+  label: string;
+}
 
 @Component({
   selector: "app-first",
@@ -7,8 +16,52 @@ import { Router } from "@angular/router";
   styleUrls: ["./first.component.css"]
 })
 export class FirstComponent implements OnInit {
-  constructor(private router: Router) {}
+  gameDefinitions: MainGameDefinition[];
+  gameDefinition: MainGameDefinition;
+  selection = <Selection>{ value: 0, label: "N/A"};
+
+  constructor(
+    private router: Router,
+    private api: DefaultService,
+    private cdRef: ChangeDetectorRef,
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit() {
+    this.api
+      .privateGameDefinitionAllGet()
+      .subscribe((gamedefinitions: MainGameDefinition[]) => {
+        this.gameDefinitions = gamedefinitions;
+        if (gamedefinitions.length > 0) {
+          this.gameDefinition = gamedefinitions[0];
+          this.selection.total = gamedefinitions.length;
+        }
+        this.updateSelection(0);
+        this.spinner.hide();
+      });
+  }
+
+  updateSelection(position: number) {
+    if (position < 0) {
+      position = 0;
+    }
+
+    if (position >= this.selection.total) {
+      position = this.selection.total - 1;
+    }
+
+    this.selection.position = position;
+    this.selection.value = Math.floor((position+1) / this.selection.total * 100);
+    this.selection.label = position + 1 + " of " + this.selection.total;
+
+    this.gameDefinition = this.gameDefinitions[position];
+  }
+
+  left() {
+    this.updateSelection(this.selection.position - 1);
+  }
+
+  right() {
+    this.updateSelection(this.selection.position + 1);
   }
 }
