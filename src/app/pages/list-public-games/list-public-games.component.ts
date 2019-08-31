ede3ab0dc8de14b6fb9ea656e86394765ea90a3f
Hamilton Lima <hamilton.lima@gmail.com>
2019-08-31 10:00:52 -0400

    Adds first version of list public games

--- src/app/pages/list-public-games/list-public-games.component.ts
+++ src/app/pages/list-public-games/list-public-games.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { DefaultService, ModelActiveMatch } from 'src/app/sdk';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { DefaultService, ModelActiveMatch, ModelAvailableMatch, ModelMatch } from "src/app/sdk";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-list-public-games',
-  templateUrl: './list-public-games.component.html',
-  styleUrls: ['./list-public-games.component.css']
+  selector: "app-list-public-games",
+  templateUrl: "./list-public-games.component.html",
+  styleUrls: ["./list-public-games.component.css"]
 })
 export class ListPublicGamesComponent implements OnInit {
-
   matches: Array<ModelActiveMatch> = [];
 
   constructor(private api: DefaultService, private router: Router) {}
 
   ngOnInit() {
-    this.api.privateAvailableMatchPublicGet().subscribe((matches: Array<ModelActiveMatch>) => {
-      console.log("matches", matches);
-      this.matches = matches;
-    });
+    this.api
+      .privateAvailableMatchPublicGet()
+      .subscribe((matches: Array<ModelActiveMatch>) => {
+        console.log("matches", matches);
+        this.matches = matches;
+      });
   }
 
+  play(matchID: number) {
+    console.log("play", matchID);
+    const request: ModelAvailableMatch = {  };
+    this.api.privatePlayIdPost(matchID).subscribe((match: ModelMatch) => {
+      console.log("joinned match", match);
+      this.router.navigate(["watch", match.id]);
+    }); 
+  }
 }
