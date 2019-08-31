521ad29a4a34745dd888823cc37f11328a12854d
Hamilton Lima <hamilton.lima@gmail.com>
2019-08-31 10:00:35 -0400

    Updates SDK

--- src/app/sdk/api/default.service.ts
+++ src/app/sdk/api/default.service.ts
@@ -668,57 +668,6 @@ export class DefaultService {
     }
 
     /**
-     * request to play a match
-     * 
-     * @param request AvailableMatch
-     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
-     * @param reportProgress flag to report request and response progress.
-     */
-    public internalPlayPost(request: ModelAvailableMatch, observe?: 'body', reportProgress?: boolean): Observable<ModelMatch>;
-    public internalPlayPost(request: ModelAvailableMatch, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<ModelMatch>>;
-    public internalPlayPost(request: ModelAvailableMatch, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<ModelMatch>>;
-    public internalPlayPost(request: ModelAvailableMatch, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
-        if (request === null || request === undefined) {
-            throw new Error('Required parameter request was null or undefined when calling internalPlayPost.');
-        }
-
-        let headers = this.defaultHeaders;
-
-        // authentication (ApiKeyAuth) required
-        if (this.configuration.apiKeys["Authorization"]) {
-            headers = headers.set('Authorization', this.configuration.apiKeys["Authorization"]);
-        }
-
-        // to determine the Accept header
-        let httpHeaderAccepts: string[] = [
-            'application/json'
-        ];
-        let httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
-        if (httpHeaderAcceptSelected != undefined) {
-            headers = headers.set("Accept", httpHeaderAcceptSelected);
-        }
-
-        // to determine the Content-Type header
-        let consumes: string[] = [
-            'application/json'
-        ];
-        let httpContentTypeSelected:string | undefined = this.configuration.selectHeaderContentType(consumes);
-        if (httpContentTypeSelected != undefined) {
-            headers = headers.set("Content-Type", httpContentTypeSelected);
-        }
-
-        return this.httpClient.post<ModelMatch>(`${this.basePath}/internal/play`,
-            request,
-            {
-                withCredentials: this.configuration.withCredentials,
-                headers: headers,
-                observe: observe,
-                reportProgress: reportProgress
-            }
-        );
-    }
-
-    /**
      * returns application health check information
      * 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
@@ -1433,6 +1382,53 @@ export class DefaultService {
         return this.httpClient.get<ModelMatch>(`${this.basePath}/private/match-single`,
             {
                 params: queryParameters,
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * request to play a match
+     * 
+     * @param id AvailableMatch id
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public privatePlayIdPost(id: number, observe?: 'body', reportProgress?: boolean): Observable<ModelMatch>;
+    public privatePlayIdPost(id: number, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<ModelMatch>>;
+    public privatePlayIdPost(id: number, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<ModelMatch>>;
+    public privatePlayIdPost(id: number, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+        if (id === null || id === undefined) {
+            throw new Error('Required parameter id was null or undefined when calling privatePlayIdPost.');
+        }
+
+        let headers = this.defaultHeaders;
+
+        // authentication (ApiKeyAuth) required
+        if (this.configuration.apiKeys["Authorization"]) {
+            headers = headers.set('Authorization', this.configuration.apiKeys["Authorization"]);
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        let httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set("Accept", httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        let consumes: string[] = [
+            'application/json'
+        ];
+
+        return this.httpClient.post<ModelMatch>(`${this.basePath}/private/play/${encodeURIComponent(String(id))}`,
+            null,
+            {
                 withCredentials: this.configuration.withCredentials,
                 headers: headers,
                 observe: observe,
