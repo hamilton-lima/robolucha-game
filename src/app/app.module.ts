a371d11bf1db26fae5c1656aad7b08486341defb
Hamilton Lima <hamilton.lima@gmail.com>
2019-09-02 19:20:14 -0400

    Removes header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
 import { BodyComponent } from "./body/body.component";
 import { LoginActivate } from "./login.activate.service";
 import { NotFoundComponent } from "./not-found/not-found.component";
@@ -116,7 +115,6 @@ export function apiConfigFactory(): Configuration {
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     BodyComponent,
     NotFoundComponent,
     SetupComponent,
