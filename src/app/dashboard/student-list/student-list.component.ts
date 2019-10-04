3e138221995f4ebd0a16ab7ed157cfeb26f122d5
Hamilton Lima <hamilton.lima@gmail.com>
2019-10-04 14:27:06 -0400

    Fixes student list

--- src/app/dashboard/student-list/student-list.component.ts
+++ src/app/dashboard/student-list/student-list.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DefaultService, ModelStudentResponse } from "src/app/sdk";
 
 @Component({
-  selector: 'app-student-list',
-  templateUrl: './student-list.component.html',
-  styleUrls: ['./student-list.component.scss']
+  selector: "app-student-list",
+  templateUrl: "./student-list.component.html",
+  styleUrls: ["./student-list.component.scss"]
 })
 export class StudentListComponent implements OnInit {
   classroomID: number;
+  students: ModelStudentResponse[] = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private api: DefaultService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.classroomID = Number.parseInt(this.route.snapshot.paramMap.get("id"));
+    this.api
+      .dashboardClassroomStudentsIdGet(this.classroomID)
+      .subscribe(response => {
+        this.students = response;
+      });
   }
-
 }
