e7fcb0af1771a0ca63843ecc426c042d4a8edee5
Hamilton Lima <hamilton.lima@gmail.com>
2019-10-07 18:16:05 -0400

    Adds buttons to classroom card

--- src/app/dashboard/shared/classroom-card/classroom-card.component.ts
+++ src/app/dashboard/shared/classroom-card/classroom-card.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ModelClassroom } from 'src/app/sdk';
+import { Component, OnInit, Input, Output } from "@angular/core";
+import { ModelClassroom } from "src/app/sdk";
+import { EventEmitter } from "events";
 
 @Component({
-  selector: 'app-classroom-card',
-  templateUrl: './classroom-card.component.html',
-  styleUrls: ['./classroom-card.component.scss']
+  selector: "app-classroom-card",
+  templateUrl: "./classroom-card.component.html",
+  styleUrls: ["./classroom-card.component.scss"]
 })
 export class ClassroomCardComponent implements OnInit {
-  
-  @Input() classroom: ModelClassroom;
-  constructor() { }
+  @Output() onStudents = new EventEmitter();
+  @Output() onAssignments = new EventEmitter();
 
-  ngOnInit() {
+  @Input() classroom: ModelClassroom;
+  constructor() {}
+
+  ngOnInit() {}
+
+  triggerOnStudents() {
+    this.onStudents.emit(null);
   }
 
+  triggerOnAssignments() {
+    this.onAssignments.emit(null);
+  }
 }
