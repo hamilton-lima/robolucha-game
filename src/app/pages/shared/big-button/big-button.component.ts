68f1d962d6d8ebf172720adc3a685b01d701b806
Hamilton Lima <hamilton.lima@gmail.com>
2019-11-29 20:43:38 -0500

    Updates game card layout

--- src/app/pages/shared/big-button/big-button.component.ts
+++ src/app/pages/shared/big-button/big-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-button',
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigButtonComponent implements OnInit {
 
+  @Input() minWidth;
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {  }
 
 }
