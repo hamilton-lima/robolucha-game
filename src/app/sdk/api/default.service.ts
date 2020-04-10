7f8e8b0a94698ec1aff6fc0028acb60d269ac17c
Hamilton Lima <hamilton.lima@gmail.com>
2020-04-10 12:11:38 -0400

    Update client SDK

--- src/app/sdk/api/default.service.ts
+++ src/app/sdk/api/default.service.ts
@@ -19,6 +19,7 @@ import { CustomHttpUrlEncodingCodec }                        from '../encoder';
 import { Observable }                                        from 'rxjs/Observable';
 
 import { ModelActiveMatch } from '../model/modelActiveMatch';
+import { ModelActivity } from '../model/modelActivity';
 import { ModelAvailableMatch } from '../model/modelAvailableMatch';
 import { ModelClassroom } from '../model/modelClassroom';
 import { ModelConfig } from '../model/modelConfig';
@@ -834,6 +835,48 @@ export class DefaultService {
         ];
 
         return this.httpClient.get<any>(`${this.basePath}/internal/ready`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * find existing activities
+     * 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public privateActivityGet(observe?: 'body', reportProgress?: boolean): Observable<Array<ModelActivity>>;
+    public privateActivityGet(observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<Array<ModelActivity>>>;
+    public privateActivityGet(observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<Array<ModelActivity>>>;
+    public privateActivityGet(observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        let headers = this.defaultHeaders;
+
+        // authentication (ApiKeyAuth) required
+        if (this.configuration.apiKeys["Authorization"]) {
+            headers = headers.set('Authorization', this.configuration.apiKeys["Authorization"]);
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        let httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set("Accept", httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        let consumes: string[] = [
+            'application/json'
+        ];
+
+        return this.httpClient.get<Array<ModelActivity>>(`${this.basePath}/private/activity`,
             {
                 withCredentials: this.configuration.withCredentials,
                 headers: headers,
