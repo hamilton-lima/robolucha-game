25b7e07254925cd442861063ad8ff16b96d8bb6c
Hamilton Lima <hamilton.lima@gmail.com>
2020-06-21 18:02:53 -0400

    Updates main page user interface

--- generate-api-client.sh
+++ generate-api-client.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "swagger-codegen-cli.jar" ]; then
     echo "downloading swagger-codegen-cli.jar"
-    wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O swagger-codegen-cli.jar
+    wget https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O swagger-codegen-cli.jar
 fi
 
 java -jar swagger-codegen-cli.jar generate --output ./src/app/sdk --lang typescript-angular --input-spec ../robolucha-api/docs/swagger.yaml
