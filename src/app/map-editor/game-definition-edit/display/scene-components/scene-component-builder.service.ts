b501a3518266d15112535d5f68d4fcdfc0d71469
Hamilton Lima <hamilton.lima@gmail.com>
2020-11-02 09:56:05 -0500

    Edits a single scene component

--- src/app/map-editor/game-definition-edit/display/scene-components/scene-component-builder.service.ts
+++ src/app/map-editor/game-definition-edit/display/scene-components/scene-component-builder.service.ts
@@ -1,23 +1,27 @@
-import { Injectable } from '@angular/core';
-import { ModelSceneComponent } from 'src/app/sdk';
+import { Injectable } from "@angular/core";
+import { ModelSceneComponent } from "src/app/sdk";
+import { ModelSceneComponentEditWrapper } from "../../game-definition-edit-mediator.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SceneComponentBuilderService {
   x = 0;
   y = 0;
   readonly WIDTH = 60;
   readonly HEIGHT = 60;
-  readonly TYPES =  ["wall", "region"];
+  readonly TYPES = ["wall", "region"];
   readonly DEFAULT_TYPE = 0;
+  id = 1;
 
-  getTypes(){
+  constructor() {}
+
+  getTypes() {
     return this.TYPES;
   }
 
   build(): ModelSceneComponent {
-    const result = <ModelSceneComponent> {
+    const result = <ModelSceneComponent>{
       alpha: 1,
       blockMovement: true,
       codes: [],
@@ -31,16 +35,30 @@ export class SceneComponentBuilderService {
       width: this.WIDTH,
       x: this.x,
       y: this.y,
-      color: "#00FF00"
-    }
+      color: "#00FF00",
+    };
 
     this.x = result.x + this.WIDTH;
     this.y = result.y;
-    
+
     return result;
   }
 
- constructor() { }
+  // Build list with local ID to allow reference for edit
+  buildWrapperList(
+    gameComponents: ModelSceneComponent[]
+  ): ModelSceneComponentEditWrapper[] {
+    return gameComponents.map((component) => this.buildWrapper(component));
+  }
+
+  buildWrapper(component: ModelSceneComponent): ModelSceneComponentEditWrapper {
+    return <ModelSceneComponentEditWrapper>{
+      id: this.id++,
+      component: component,
+    };
+  }
+
+  unWrapList(wrapped: ModelSceneComponentEditWrapper[]): ModelSceneComponent[] {
+    return wrapped.map((w) => w.component);
+  }
 }
-
-
--- src/app/map-editor/game-definition-edit/editor/single-scene-component-editor/single-scene-component-editor.component.ts
+++ src/app/map-editor/game-definition-edit/editor/single-scene-component-editor/single-scene-component-editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ModelCode, ModelGameComponent, ModelSceneComponent } from 'src/app/sdk';
+import { GameDefinitionEditMediatorService, ModelGameComponentEditWrapper, ModelSceneComponentEditWrapper } from '../../game-definition-edit-mediator.service';
 
 @Component({
   selector: 'app-single-scene-component-editor',
@@ -7,9 +10,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleSceneComponentEditorComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  component: ModelSceneComponent;
+
+  form = this.formBuilder.group({
+    type: ["", Validators.required],
+    color: ["", Validators.required],
+    x: ["", [Validators.required, Validators.pattern("^[0-9]*$")]],
+    y: ["", [Validators.required, Validators.pattern("^[0-9]*$")]],
+    width: ["", [Validators.required, Validators.pattern("^[0-9]*$")]],
+    height: ["", [Validators.required, Validators.pattern("^[0-9]*$")]],
+  });
+
+  helpFile: string;
+  editors: { event: string; label: string; }[];
+  codes: ModelCode[];
+
+  constructor(
+    private mediator: GameDefinitionEditMediatorService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.mediator.onEditSceneComponent.subscribe(
+      (wrapper: ModelSceneComponentEditWrapper) => {
+        this.id = wrapper.id;
+        this.component = wrapper.component;
+        this.codes = this.component.codes;
+        this.form.patchValue(this.component);
+      }
+    );
+
+    this.form.valueChanges.subscribe(() => {
+      this.save();
+    });
+
+    this.helpFile = "help/code_editor_help";
+    this.editors = [
+      { event: "onRepeat", label: "On repeat" },
+      { event: "onStart", label: "On start" },
+      { event: "onHitWall", label: "On hit wall" },
+      { event: "onFound", label: "On found" },
+      { event: "onGotDamage", label: "On got damage" },
+      { event: "onHitOther", label: "On hit other" },
+    ];
+
+  }
+
+  save() {
+    if (this.form.valid) {
+      const component = <ModelGameComponent>{
+        type: this.form.get("type").value,
+        color: this.form.get("color").value,
+        x: Number.parseInt(this.form.get("x").value),
+        y: Number.parseInt(this.form.get("y").value),
+        width: Number.parseInt(this.form.get("width").value),
+        height: Number.parseInt(this.form.get("height").value),
+        codes: this.codes
+      };
+
+      const value = <ModelSceneComponentEditWrapper>{
+        id: this.id,
+        component: component,
+      };
+      this.mediator.onUpdateSceneComponent.next(value);
+    }
+  }
+
+  updateCode(codes: ModelCode[]){
+    this.codes = codes;
+    this.save();
   }
 
 }
