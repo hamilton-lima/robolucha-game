c9a71f5b3ed48f62c8d1d2995476b33125b0ac9e
Hamilton Lima <hamilton.lima@gmail.com>
2020-11-16 00:21:34 -0500

    Reformat code

---
+++ src/app/shared/code-blockly/code-blockly.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class BlocklyService {
+  defaultToolbox() {
+    const toolbox = `
+        <xml>
+          <category name="Commands" expanded="true">
+              <block type="move" />
+              <block type="fire" />
+              <block type="turn" />
+              <block type="turnGun" />
+              <block type="reset" />
+              <block type="debug" />
+          </category>
+  
+          <category name="Variables" custom="VARIABLE"></category>
+          <sep></sep>
+  
+          <category name="Math">
+            <block type="math_number"></block>
+            <block type="math_arithmetic"></block>
+            <block type="math_single"></block>
+            <block type="math_constant"></block>
+            <block type="math_random_int"></block>
+          </category>
+  
+          <category name="Control">
+            <block type="controls_if"></block>
+            <block type="controls_ifelse"></block>
+            <block type="controls_whileUntil"></block>
+            <block type="controls_for"></block>
+            <block type="controls_forEach"></block>
+            <block type="controls_flow_statements"></block>
+          </category>
+  
+          <category name="Logic">
+            <block type="logic_compare"></block>
+            <block type="logic_operation"></block>
+            <block type="logic_boolean"></block>
+            <block type="logic_negate"></block>
+          </category>
+        </xml>`;
+
+    return toolbox;
+  }
+}
