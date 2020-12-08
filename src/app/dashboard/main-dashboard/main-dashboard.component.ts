42871c7ea68d9bb8babf8d8dd5931b434d785459
Hamilton Lima <hamilton.lima@gmail.com>
2020-12-07 20:04:05 -0500

    Organize dashboard screen

--- src/app/dashboard/main-dashboard/main-dashboard.component.ts
+++ src/app/dashboard/main-dashboard/main-dashboard.component.ts
@@ -32,12 +32,10 @@ export class MainDashboardComponent implements OnInit {
   classrooms() {
     this.events.click(this.page, "dashboard/classrooms");
     this.router.navigate(["dashboard/classrooms"]);
-    this.sidenav.close();
   }
 
   activities() {
     this.events.click(this.page, "dashboard/activities");
     this.router.navigate(["dashboard/activities"]);
-    this.sidenav.close();
   }
 }
